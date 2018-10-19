e90ee8c6c910570c28a3f309049ec575a9f98871
karan singh <singhatwalkaran@gmail.com>
2018-10-19 05:12:10 -0400

    chakk fer

---
+++ midterm1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(midterm1.Startup))]
+namespace midterm1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
